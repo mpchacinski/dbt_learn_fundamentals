e99b51b8c243f15e670aee30d2ddda43ccbfecdb
mpchacinski <mpchacinski@gmail.com>
2026-06-25 12:52:18 +0200

    added fct_orders table

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,33 @@
+
+with payments as (
+    
+    select
+        order_id,
+        sum(case when status = 'success' then amount end) as amount
+    from {{ ref('stg_stripe__payments') }}
+    group by order_id
+
+),
+
+orders as (
+
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+
+),
+
+final as (
+    
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(payments.amount, 0) as amount
+
+    from orders
+
+    left join payments 
+        on orders.order_id = payments.order_id
+
+)
+
+select * from final
