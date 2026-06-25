45f958e8e6b79724436dd427469063f9a81e5571
mpchacinski <mpchacinski@gmail.com>
2026-06-25 12:23:14 +0200

    added 2 staging models

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from DATAEXPERT_STUDENT.MPCHACINSKI11351.RAW_JS_CUSTOMERS
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from DATAEXPERT_STUDENT.MPCHACINSKI11351.RAW_JS_ORDERS
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
